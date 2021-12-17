bc933cd5c3f5dcaa617853a0721a90bebb105a01
rubyosborne <ruby.osborne@gmail.com>
2021-12-17 13:27:17 +1300

    initial commit

---
+++ ItemsApi/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ItemsApi.Models;
+
+namespace ItemsApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Item> items = new List<Item>()
+        {
+            new Item(1, "Phone", 2, "Budapest", 2),
+            new Item(2, "Laptop", 3, "Nigeria", 3),
+            new Item(3, "Headphones", 4, "Lagos", 4),
+            new Item(4, "Drink Bottle", 5, "Asaba", 5),
+            new Item(5, "Peanut Butter", 6, "Oko", 6)
+        };
+
+
+        //GET api/item/5
+        [HttpGet("{id}", Name = "Get")]
+        public Item Get(int id)
+        {
+            Item item = items.Find(f => f.id == id);
+            return item;
+        }
+
+
+
+        [HttpPost]
+        public List<Item> Post([FromBody] Item item)
+        {
+            items.Add(item);
+            return items;
+        }
+
+
+
+        [HttpPut("{id}")]
+        public List<Item> Put(int id, [FromBody] Item item)
+        {
+            Item ItemToUpdate = items.Find(f => f.id == id);
+            int index = items.IndexOf(ItemToUpdate);
+
+            items[index].name = item.name;
+            items[index].price = item.price;
+            items[index].location = item.location;
+            items[index].quantityAvailable = item.quantityAvailable;
+
+            return items;
+        }
+
+
+        [HttpDelete("{id}")]
+        public List<Item> Delete(int id)
+        {
+            Item item = items.Find(f => f.id == id);
+            items.Remove(item);
+            return items;
+        }
+    }
+
+
+}
---
+++ ItemsApi/Models/items.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ItemsApi.Models
+{
+
+    public class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string location { get; set; }
+        public int quantityAvailable { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(int id, string name, int price, string location, int quantityAvailable)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.location = location;
+            this.quantityAvailable = quantityAvailable;
+        }
+    }
+}
